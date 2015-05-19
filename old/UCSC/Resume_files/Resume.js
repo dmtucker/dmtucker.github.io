c46925a76f29d04eef65e1211ae1664018a4711c
David Tucker <dmtucker@users.noreply.github.com>
2015-05-19 01:08:57 -0700

    Remove the old site which now has its own branch.

--- old/UCSC/Resume_files/Resume.js
+++
@@ -1,7 +0,0 @@
-// Created by iWeb 3.0.2 local-build-20110212
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Resume_files/ResumeMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');performPostEffectsFixups()}
