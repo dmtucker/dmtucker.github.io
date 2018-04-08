bfc8c452af544a0ec6c37b3dcf496a10de3ad9f5
David Tucker <david@tucker.name>
2018-04-08 09:51:24 -0700

    Add the old iweb site to the history

---
+++ UCSC/Resume_files/Resume.js
@@ -0,0 +1,7 @@
+// Created by iWeb 3.0.2 local-build-20110212
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Resume_files/ResumeMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');performPostEffectsFixups()}
